687be87159e78d3be6cfa433e496be5f02549dd9
Aaron <aamclean16@gmail.com>
2024-09-24 14:04:49 -0600

    updated Modelsim files for timed counter

--- sim/hw-4/timed_counter.vhd
+++ sim/hw-4/timed_counter.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity timed_counter is
     generic(
@@ -15,6 +16,28 @@ end entity timed_counter;
 
 architecture timed_counter_arch of timed_counter is
 
+    constant count_max : integer := count_time/clk_period;
+
+    signal cnt : integer;
+
     begin
+
+	CNTR1 : process(clk, enable)
+	    begin
+	    	if (rising_edge(clk)) then
+		    if (enable) then
+			if (cnt = count_max) then
+			    cnt <= 0;
+			    done <= true;
+			else
+			    cnt <= cnt + 1;
+			    done <= false;
+			end if;
+		    else
+			cnt <= 0;
+			done <= false;
+		    end if;
+	    	end if;
+	end process;
 
 end architecture timed_counter_arch;
