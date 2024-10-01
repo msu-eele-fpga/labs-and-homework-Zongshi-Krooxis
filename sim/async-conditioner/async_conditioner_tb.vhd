9de781284dcd4dee42d194cf5c4fbfcb5b5c133a
Aaron <aamclean16@gmail.com>
2024-10-01 12:35:24 -0600

    add Async Conditioner and One Pulse Test Bench starting files

---
+++ sim/async-conditioner/async_conditioner_tb.vhd
@@ -0,0 +1,10 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity async_conditioner_tb is
+end entity;
+
+architecture async_conditioner_tb_arch of async_conditioner_tb is
+    begin
+end architecture;
---
+++ sim/async-conditioner/one_pulse_tb.vhd
@@ -0,0 +1,10 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity one_pulse_tb is
+end entity;
+
+architecture one_pulse_tb_arch of one_pulse_tb is
+    begin
+end architecture;
