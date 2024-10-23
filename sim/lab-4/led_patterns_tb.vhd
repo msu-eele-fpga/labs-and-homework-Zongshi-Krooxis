34c3ecf3a3150ac232a08e5521fa035f40b257ec
Aaron <aamclean16@gmail.com>
2024-10-23 09:11:51 -0600

    added lab 4 sim file. Including the LED_Patterns tb

---
+++ sim/lab-4/led_patterns_tb.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.assert_pkg.all;
+use work.print_pkg.all;
+use work.tb_pkg.all;
+
+entity led_patterns_tb is
+end entity;
+
+architecture led_patterns_tb_arch of led_patterns_tb is
+    begin
+end architecture;
